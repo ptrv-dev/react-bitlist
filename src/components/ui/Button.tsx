2d4b8cc2904623c17462d71aadf91d155b06f6fc
ptrv-dev <petrov99119@gmail.com>
2023-09-03 16:52:14 +0300

    ui: button

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,30 @@
+import { FC, HTMLProps } from 'react';
+import cn from 'classnames';
+
+interface Props extends HTMLProps<HTMLButtonElement> {
+  type?: 'button' | 'submit' | 'reset' | undefined;
+  variant?: 'primary' | 'secondary' | 'outlined';
+}
+
+const Button: FC<Props> = ({
+  className,
+  variant = 'primary',
+  children,
+  ...props
+}) => {
+  return (
+    <button
+      className={cn(
+        'flex outline-none text-lg font-medium py-[10px] px-6 rounded-md',
+        { 'bg-gradient-primary text-dark': variant === 'primary' },
+        { 'bg-gradient-dark': variant === 'secondary' },
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
